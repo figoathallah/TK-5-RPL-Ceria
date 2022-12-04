8d0258153f2e115dd2e9e1650bcf01f325a96c61
figoathallah <figoirawan02@gmail.com>
2022-12-04 11:01:08 +0700

    test route

--- api/views.py
+++ api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/movies',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns the movies that are currently showing'
+        },
+
+        {
+            'Endpoint': '/movies/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single movie object to view details on'
+        },
+
+        {
+            'Endpoint': '/movies/create',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates a new movie in the database'
+        },
+
+        {
+            'Endpoint': '/movies/id/update',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Updates an existing movie with new data'
+        },
+
+        {
+            'Endpoint': '/movies/id/delete',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes a movie in the database'
+        },
+    ]
+    return JsonResponse(routes, safe=False)
